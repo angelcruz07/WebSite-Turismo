43420c77c0245b7880cb413143aa8e0d28c78485
angelcruz07 <angelcruzsanchez126@gmail.com>
2023-09-20 11:04:05 -0500

    Responsive Agregado

--- php/events.php
+++ php/events.php
@@ -1,27 +1,27 @@
 <section class="events" id="events">
 <h2>Eventos</h2>
-    <div class="container">
-        <div class="item">
+    <div class="container-events">
+        <div class="card-event">
             <img src="./assets/img/iglesia sult.jpg" alt="Imagen 1">
-            <div class="info">
+            <div class="description-event">
                 <!-- Información para la primera imagen -->
-                <h2>Nombre evento</h2>
+                <h3>Nombre evento</h3>
                 <p>Descripción corta.</p>
             </div>
         </div>
-        <div class="item">
+        <div class="card-event">
             <img src="./assets/img/iglesia sult.jpg" alt="Imagen 2">
-            <div class="info">
+            <div class="description-event">
                 <!-- Información para la segunda imagen -->
-                <h2>Nombre evento</h2>
+                <h3>Nombre evento</h3>
                 <p>Descripción corta.</p>
             </div>
         </div>
-        <div class="item">
+        <div class="card-event">
             <img src="./assets/img/iglesia sult.jpg" alt="Imagen 3">
-            <div class="info">
+            <div class="description-event">
                 <!-- Información para la tercera imagen -->
-                <h2>Nombre evento</h2>
+                <h3>Nombre evento</h3>
                 <p>Descripción corta.</p>
             </div>
         </div>
