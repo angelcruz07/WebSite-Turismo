921513e358517e677e30465c32dae768d1cd4440
Lizandro Antonio Santos <CodeVerseBackend@gmail.com>
2023-10-09 21:37:32 -0600

    Se ha agregado a suggestions la funcion flip card pero falta darle un buen diseño a el elemnto a

--- php/suggestions.php
+++ php/suggestions.php
@@ -7,34 +7,62 @@ $redirection = "http://" . $_SERVER['HTTP_HOST'] . "/WebSite-Turismo";
 
   <h2 class="title-suggetions">¿Que deseas conocer?</h2>
 
-  <div class="container_sugetions">
-    <div class="card_suggetion">
-      <img src="<?php echo $url ?>antiguo sultepec.jpg" alt="Cultura sultepequense">
-      <div class="text_sugetion">
-        <a href="#" class="hover-text">Leer más</a>
-      </div>
-      <h2>Historia</h2>
+  <div class="container_sugetions">  
+
+  <div class="card">
+        <div class="face front">
+                <img src="<?php echo $url ?>antiguo sultepec.jpg" alt="Cultura sultepequense">
+                <h3>Historia</h3>
+        </div>
+        <div class="face back">
+            <h3>Historia</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In reiciendis neque nam quibusdam qui cupiditate esse!</p>
+            <div class="link"> 
+            <a href="#" class="hover-text">Leer más</a>
+            </div>
+        </div>
+    </div> 
+     
+    <div class="card">
+        <div class="face front">
+                <img src="<?php echo $url ?>galletas.jpeg" alt="Gastronomia sultepequence">
+                <h3>Gastronomia</h3>
+        </div>
+        <div class="face back">
+            <h3>Gastronomia</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur ipsum numquam modi dolorem. Nobis, quasi fuga!</p>
+            <div class="link"> 
+            <a href="<?php echo $redirection?>/php/gastronomy.php" class="hover-text">Leer más</a>
+            </div>
+        </div>
+    </div> 
+     
+    <div class="card">
+        <div class="face front">
+                <img src="<?php echo $url ?>minero sultepec.jpg" alt="Que visitar en sultepec">
+                <h3>Lugares</h3>
+        </div>
+        <div class="face back">
+            <h3>Lugares</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, magnam consequuntur veritatis voluptas doloremque nemo perspiciatis.</p>
+            <div class="link">
+            <a href="#" class="hover-text">Leer más</a>
+            </div>
+        </div>
+    </div> 
+     
+    <div class="card">
+        <div class="face front">
+                <img src="<?php echo $url ?>hospedaje.jpeg" alt="Donde hospedarte en sultepec">
+                <h3>Hospedaje</h3>
+        </div>
+        <div class="face back">
+            <h3>Hospedaje</h3>
+           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum molestias magnam quae odit doloremque officia quisquam.</p>
+            <div class="link">
+            <a href="#" class="hover-text">Leer más</a>
+            </div>
+        </div>
     </div>
-    <div class="card_suggetion">
-      <img src="<?php echo $url ?>galletas.jpeg" alt="Gastronomia sultepequence">
-      <div class="text_sugetion">
-        <a href="<?php echo $redirection?>/php/gastronomy.php" class="hover-text">Leer más</a>
-      </div>
-      <h2>Gastronomía</h2>
-    </div>
-    <div class="card_suggetion">
-      <img src="<?php echo $url ?>minero sultepec.jpg" alt="Que visitar en sultepec">
-      <div class="text_sugetion">
-        <a href="#" class="hover-text">Leer más</a>
-      </div>
-      <h2>Lugares</h2>
-    </div>
-    <div class="card_suggetion">
-      <img src="<?php echo $url ?>hospedaje.jpeg" alt="Donde hospedarte en sultepec">
-      <div class="text_sugetion">
-        <a href="#" class="hover-text">Leer más</a>
-      </div>
-      <h2>Hospedaje</h2>
-    </div>
-  </div>
+     
 </section>
