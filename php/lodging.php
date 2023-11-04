09cd50fa26993c209b9c8a11a018b48f150d546a
Angel Cruz <angelcruzsanchez126@gmail.com>
2023-11-04 17:56:07 -0600

    Rediseño de las seccion de hospedaje

--- php/lodging.php
+++ php/lodging.php
@@ -3,7 +3,7 @@ require "../partials/header2.php";
 require "../partials/navbar.php";
 require "../partials/scroll-top.php";
 
-// Cambio de diseño
+
 ?>
 <link rel="stylesheet" href="<?php echo $url; ?>/assets/css/lodging.css" type="text/css">
 </head>
@@ -23,28 +23,98 @@ require "../partials/scroll-top.php";
     <div class="container-lodging">
       <div class="lodging-box">
         <div class="lodging-image">
-          <img src="<?php  echo $url?>/assets/img/hotel.jpg" alt="Imagen del hotel">
-          <div class="overlay">
-            <p> Calificacion</p>
-          </div>
+          <img src="<?php echo $url ?>/assets/img/descarga.jpg" alt="Imagen del hotel">
+
         </div>
         <div class="description-lodging">
-          <h3>Nombre del hospedaje</h3>
-          <p>Descripcion del hospedaje</p>
-          <a href="#"><img src="<?php echo $url ?>/assets/icons/contact.svg" alt=""></a>
-        </div>
-      </div>
-      <div class="lodging-box-left">
-        <div class="lodging-image">
-          <img src="<?php  echo $url?>/assets/img/hotel.jpg" alt="Imagen del hotel">
-          <div class="overlay-left">
-            <p> Calificacion</p>
+          <h3>Hotel real</h3>
+          <p>Descripcion del hospedaje Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet omnis vel deserunt
+            distinctio, beatae quod molestias aliquam modi natus voluptas, earum maxime saepe illum possimus esse veniam
+            dolore cum nisi!
+          </p><br>
+          <div class="calf-star">
+            <img src="../assets/icons/star.svg" alt="">
+            <img src="../assets/icons/star.svg" alt="">
+            <img src="../assets/icons/star.svg" alt="">
+            <img src="../assets/icons/star-regular.svg" alt="">
+            <img src="../assets/icons/star-regular.svg" alt="">
+          </div>
+          <div class="link-lodging">
+            <a href="#">Ver en el mapa</a>
           </div>
         </div>
+      </div>
+    </div>
+    <div class="container-lodging">
+      <div class="lodging-box-left">
+        <div class="lodging-image">
+          <img src="<?php echo $url ?>/assets/img/hotel.webp" alt="Imagen del hotel">
+        </div>
         <div class="description-lodging left">
-          <h3>Nombre del hospedaje</h3>
-          <p>Descripcion del hospedaje</p>
-          <a href="#"><img src="<?php echo $url ?>/assets/icons/contact.svg" alt=""></a>
+          <h3>Hotel casa juanita</h3>
+          <p>Descripcion del hospedaje Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet omnis vel deserunt
+            distinctio, beatae quod molestias aliquam modi natus voluptas, earum maxime saepe illum possimus esse veniam
+            dolore cum nisi!
+          </p><br>
+          <div class="calf-star">
+            <img src="../assets/icons/star.svg" alt="">
+            <img src="../assets/icons/star.svg" alt="">
+            <img src="../assets/icons/star.svg" alt="">
+            <img src="../assets/icons/star-regular.svg" alt="">
+            <img src="../assets/icons/star-regular.svg" alt="">
+          </div>
+          <div class="link-lodging">
+            <a href="#">Ver en el mapa</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container-lodging">
+      <div class="lodging-box">
+        <div class="lodging-image">
+          <img src="<?php echo $url ?>/assets/img/descarga.jpg" alt="Imagen del hotel">
+
+        </div>
+        <div class="description-lodging">
+          <h3>Hotel real</h3>
+          <p>Descripcion del hospedaje Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet omnis vel deserunt
+            distinctio, beatae quod molestias aliquam modi natus voluptas, earum maxime saepe illum possimus esse veniam
+            dolore cum nisi!
+          </p><br>
+          <div class="calf-star">
+            <img src="../assets/icons/star.svg" alt="">
+            <img src="../assets/icons/star.svg" alt="">
+            <img src="../assets/icons/star.svg" alt="">
+            <img src="../assets/icons/star-regular.svg" alt="">
+            <img src="../assets/icons/star-regular.svg" alt="">
+          </div>
+          <div class="link-lodging">
+            <a href="#">Ver en el mapa</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container-lodging">
+      <div class="lodging-box-left">
+        <div class="lodging-image">
+          <img src="<?php echo $url ?>/assets/img/hotel.webp" alt="Imagen del hotel">
+        </div>
+        <div class="description-lodging left">
+          <h3>Hotel casa juanita</h3>
+          <p>Descripcion del hospedaje Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet omnis vel deserunt
+            distinctio, beatae quod molestias aliquam modi natus voluptas, earum maxime saepe illum possimus esse veniam
+            dolore cum nisi!
+          </p><br>
+          <div class="calf-star">
+            <img src="../assets/icons/star.svg" alt="">
+            <img src="../assets/icons/star.svg" alt="">
+            <img src="../assets/icons/star.svg" alt="">
+            <img src="../assets/icons/star-regular.svg" alt="">
+            <img src="../assets/icons/star-regular.svg" alt="">
+          </div>
+          <div class="link-lodging">
+            <a href="#">Ver en el mapa</a>
+          </div>
         </div>
       </div>
     </div>
