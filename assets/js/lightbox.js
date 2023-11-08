ca04b0872634738115ef39a84e4f7dafa6d49e88
angelcruz07 <angelcruzsanchez126@gmail.com>
2023-11-08 10:04:40 -0600

    Solucion de eventos de link de eventos progresada

--- assets/js/lightbox.js
+++ assets/js/lightbox.js
@@ -9,3 +9,15 @@ $(document).ready(function() {
     });
 });
 
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Obtén una colección de elementos con la clase "back"
+    let mapButtons = document.getElementsByClassName("back");
+  
+    // Itera a través de la colección y agrega el evento "mouseenter" a cada elemento
+    for (let i = 0; i < mapButtons.length; i++) {
+      mapButtons[i].addEventListener("mouseenter", function() {
+        let link = document.getElementsByClassName("link").style.display = "block";
+      });
+    }
+  });
