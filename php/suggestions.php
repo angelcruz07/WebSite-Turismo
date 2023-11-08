20e3973f44c32f26f0b69f46cb9b4bd6920e3ed3
Angel Cruz <angelcruzsanchez126@gmail.com>
2023-11-07 20:14:04 -0600

    Cambio en la seccion de servicios

--- php/suggestions.php
+++ php/suggestions.php
@@ -3,65 +3,66 @@ $images = "http://" . $_SERVER['HTTP_HOST'] . "/WebSite-Turismo/assets/img/";
 ?>
 
 <section id="suggestions">
-    <h2 class="title-index">¿Que deseas conocer?</h2>
-    <div class="container_sugetions">
-        <div class="card-suggetion">
-            <div class="face front">
-                <img src="<?php echo $images ?>antiguo-sultepec.jpg" alt="Cultura sultepequense" title="Cultura">
-                <h3>Historia</h3>
-            </div>
-            <div class="face back">
-                <h3>Historia</h3>
-                <p>Sultepec, tierra de tradiciones ancestrales y belleza natural, donde el pasado se entrelaza con el presente en cada rincón del pueblo.</p>
-                <div class="link">
-                    <a href="#" class="hover-text">Leer más</a>
-                </div>
-            </div>
+  <h2 class="title-index">Aqui comienza tu nueva aventura</h2>
+  <div class="container_sugetions">
+    <div class="card-suggetion">
+      <div class="face front">
+        <img src="<?php echo $images ?>antiguo-sultepec.jpg" alt="Cultura sultepequense" title="Cultura">
+        <h3>Historia</h3>
+      </div>
+      <div class="face back">
+        <h3>Historia</h3>
+        <p>Sultepec, tierra de tradiciones ancestrales y belleza natural, donde el pasado se entrelaza con el presente
+          en cada rincón del pueblo.</p>
+        <div class="link">
+          <a href="#" class="hover-text">Leer más</a>
         </div>
+      </div>
+    </div>
 
-        <div class="card-suggetion">
-            <div class="face front">
-                <img src="<?php echo $images ?>pipian-dulce.jpg" alt="Gastronomia sultepequence" title="Gastronomia">
-                <h3>Gastronomia</h3>
-            </div>
-            <div class="face back">
-                <h3>Gastronomia</h3>
-                <p>Las rosquitas o frutas de horno son una delicia tradicional de la gastronomía de Sultepec, que ha sido elaborada por generaciones desde 1890</p>
-                <div class="link">
-                    <a href="<?php echo $url ?>/php/gastronomy.php" class="hover-text">Leer más</a>
-                </div>
-            </div>
+    <div class="card-suggetion">
+      <div class="face front">
+        <img src="<?php echo $images ?>pipian-dulce.jpg" alt="Gastronomia sultepequence" title="Gastronomia">
+        <h3>Gastronomia</h3>
+      </div>
+      <div class="face back">
+        <h3>Gastronomia</h3>
+        <p>Las rosquitas o frutas de horno son una delicia tradicional de la gastronomía de Sultepec, que ha sido
+          elaborada por generaciones desde 1890</p>
+        <div class="link">
+          <a href="<?php echo $url ?>/php/gastronomy.php" class="hover-text">Leer más</a>
         </div>
+      </div>
+    </div>
 
-        <div class="card-suggetion">
-            <div class="face front">
-                <img src="<?php echo $images ?>minero sultepec.jpg" alt="Lugares Turisticos sultepec" title="Turismo">
-                <h3>Lugares</h3>
-            </div>
-            <div class="face back">
-                <h3>Lugares</h3>
-                <p>Los coloridos mercados de Sultepec son un verdadero festín para los sentidos, donde puedes encontrar productos frescos, artesanías locales y sumergirte en la vibrante cultura de este encantador pueblo<p>
-                <div class="link">
-                    <a href="<?php echo $url ?>/php/attractive.php" class="hover-text">Leer más</a>
-                </div>
-            </div>
+    <div class="card-suggetion">
+      <div class="face front">
+        <img src="<?php echo $images ?>minero sultepec.jpg" alt="Lugares Turisticos sultepec" title="Turismo">
+        <h3>Lugares</h3>
+      </div>
+      <div class="face back">
+        <h3>Lugares</h3>
+        <p>Los coloridos mercados de Sultepec son un verdadero festín para los sentidos, donde puedes encontrar
+          productos frescos, artesanías locales y sumergirte en la vibrante cultura de este encantador pueblo
+        <p>
+        <div class="link">
+          <a href="<?php echo $url ?>/php/attractive.php" class="hover-text">Leer más</a>
         </div>
+      </div>
+    </div>
 
-        <div class="card-suggetion">
-            <div class="face front">
-                <img src="<?php echo $images ?>hospedaje.jpeg" alt="Donde hospedarte en sultepec" title="Hospedajes">
-                <h3>Hospedaje</h3>
-            </div>
-            <div class="face back">
-                <h3>Hospedaje</h3>
-                <p>Los hospedajes te reciben con calidez y comodidad, brindándote una experiencia inolvidable en medio de la belleza natural de este encantador pueblo.</p>
-                <div class="link">
-                    <a href="<?php echo $url ?>/php/lodging.php" class="hover-text">Leer más</a>
-                </div>
-            </div>
+    <div class="card-suggetion">
+      <div class="face front">
+        <img src="<?php echo $images ?>hospedaje.jpeg" alt="Donde hospedarte en sultepec" title="Hospedajes">
+        <h3>Hospedaje</h3>
+      </div>
+      <div class="face back">
+        <h3>Hospedaje</h3>
+        <p>Los hospedajes te reciben con calidez y comodidad, brindándote una experiencia inolvidable en medio de la
+          belleza natural de este encantador pueblo.</p>
+        <div class="link">
+          <a href="<?php echo $url ?>/php/lodging.php" class="hover-text">Leer más</a>
         </div>
+      </div>
+    </div>
 </section>
- 
-
-
-
