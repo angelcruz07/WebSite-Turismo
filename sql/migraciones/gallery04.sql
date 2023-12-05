958530ab34d89981604623bf44d961a92216393a
LizandroBackEnd <CodeVerseBackend@gmail.com>
2023-12-05 10:17:47 -0600

    Inyecciones SQL

--- sql/migraciones/gallery04.sql
+++ sql/migraciones/gallery04.sql
@@ -1,130 +1,14 @@
--- Active: 1700014601609@@127.0.0.1@3306@tourism
-INSERT INTO
-    `gallery` (
-        `id`,
-        `type`,
-        `name`,
-        `description`,
-        `location`,
-        `image`
-    )
-VALUES (
-        1,
-        'Mirador',
-        'EL MIRADOR ',
-        'Un lugar de tranquilidad, con una excelente vista ',
-        'https://maps.app.goo.gl/xRKSsP7wLSSMSa3ZA',
-        '1700795074_MIRADOR.jpg'
-    ), (
-        2,
-        'Mirador',
-        'EL MIRADOR ',
-        'Un lugar lleno de paz para disfrutar junto a tu familia',
-        'https://maps.app.goo.gl/xRKSsP7wLSSMSa3ZA',
-        '1700795344_park_mirador.jpg'
-    ), (
-        3,
-        'Parroquia',
-        'PARROQUIA DE SAN JUAN BAUTISTA',
-        'Una parroquia muy hermosa, una gran arquitectura del siglo XVII.',
-        'https://maps.app.goo.gl/mAWJqrw1J6Rn8vev8',
-        '1700795652_juan.jpg'
-    ), (
-        4,
-        'Parroquia',
-        'SANTUARIO SEÑOR DE LA SANTA VERACRUZ',
-        'Un santuario que data del siglo XIX y alberga una imagen del Cristo Negro a la que los devotos le atribuyen varios milagros.',
-        'https://maps.app.goo.gl/T1moKtMuXBactw6g8',
-        '1700795877_veracruz.jpeg'
-    ), (
-        5,
-        'Monumentos',
-        'EL MINERO',
-        'El minero un monumento hecho a reconocimiento del pueblo con encanto Sultepec',
-        'https://maps.app.goo.gl/HCzjBLE6JaRvukxv5',
-        '1700796262_minero.jpg'
-    ), (
-        6,
-        'Parques',
-        'UNIDAD DEPORTIVA EL CHOCOLATE',
-        'No hay mejor lugar para practicar futbol que el la unidad deportiva el chocolate ',
-        'https://maps.app.goo.gl/Zt8ZEFd6CKCHBryN7',
-        '1700796603_CHOCOLATE.jpg'
-    ), (
-        7,
-        'Monumentos',
-        'LETRAS SULTEPEC ',
-        'Monumento al  municipio de Sultepec, las letras SULTEPEC ',
-        'https://maps.app.goo.gl/6xfXrgYa6Y1N2EAG7',
-        '1700797011_LETRAS.jpg'
-    ), (
-        8,
-        'Parques',
-        'PARQUE RECREATIVO EL MIRADOR',
-        'El parque perfecto para venir a disfrutar un día de campo junto a la familia, lugar con muchos arboles y juegos impresionantes. ',
-        'https://maps.app.goo.gl/7NchJaJ9GqTQn4XQ7',
-        '1700797444_parque.jpeg'
-    ), (
-        9,
-        'Parroquia',
-        'IGLESIA DE LA UNION',
-        'Una iglesia muy antigua con unas grandes decoraciones, una de las iglesias más bellas de Sultepec',
-        'https://maps.app.goo.gl/sxA9ngqvRfgi3u2t8',
-        '1700798263_UNION.png'
-    ), (
-        10,
-        '#',
-        'Santuario de la Santa Veracruz',
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua”',
-        '#',
-        '1701453531_vera.jpg'
-    ), (
-        11,
-        'Monumentos',
-        'EL MINERO',
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua”',
-        '#',
-        '1701453825_miner.jpg'
-    ), (
-        12,
-        'Parques',
-        'KIOSKO MUNICIPAL',
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua”',
-        '#',
-        '1701453890_KIOSK.jpg'
-    ), (
-        13,
-        'Monumentos',
-        'MUSEO MUNICIPAL',
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua”',
-        '#',
-        '1701453975_MUSEO.jpg'
-    ), (
-        14,
-        'Parroquia',
-        'Parroquia de sulltepec',
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua”',
-        '#',
-        '1701455235_Parroquia.jpg'
-    ), (
-        15,
-        'Mirador',
-        'Mirador',
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua”',
-        '#',
-        '1701455366_mira.jpg'
-    ), (
-        16,
-        'Parques',
-        'Cabaña',
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua”',
-        '#',
-        '1701456131_hot.jpg'
-    ), (
-        18,
-        'Parroquia',
-        'Iglesia',
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua”',
-        '#',
-        '1701456477_igle.jpg'
-    );
+INSERT INTO `gallery` (`id`, `type`, `name`, `description`, `location`, `image`) VALUES
+(1, 'Mirador', 'EL MIRADOR ', 'Un lugar de tranquilidad, con una excelente vista ', 'https://maps.app.goo.gl/xRKSsP7wLSSMSa3ZA', '1700795074_MIRADOR.jpg'),
+(2, 'Mirador', 'EL MIRADOR ', 'Un lugar lleno de paz para disfrutar junto a tu familia', 'https://maps.app.goo.gl/xRKSsP7wLSSMSa3ZA', '1700795344_park_mirador.jpg'),
+(3, 'Parroquia', 'PARROQUIA DE SAN JUAN BAUTISTA', 'Una parroquia muy hermosa, una gran arquitectura del siglo XVII.', 'https://maps.app.goo.gl/mAWJqrw1J6Rn8vev8', '1700795652_juan.jpg'),
+(4, 'Parroquia', 'SANTUARIO SEÑOR DE LA SANTA VERACRUZ', 'Un santuario que data del siglo XIX y alberga una imagen del Cristo Negro a la que los devotos le atribuyen varios milagros.', 'https://maps.app.goo.gl/T1moKtMuXBactw6g8', '1700795877_veracruz.jpeg'),
+(5, 'Monumentos', 'EL MINERO', 'El minero un monumento hecho a reconocimiento del pueblo con encanto Sultepec', 'https://maps.app.goo.gl/HCzjBLE6JaRvukxv5', '1700796262_minero.jpg'),
+(6, 'Parques', 'UNIDAD DEPORTIVA EL CHOCOLATE', 'No hay mejor lugar para practicar futbol que el la unidad deportiva el chocolate ', 'https://maps.app.goo.gl/Zt8ZEFd6CKCHBryN7', '1700796603_CHOCOLATE.jpg'),
+(7, 'Monumentos', 'LETRAS SULTEPEC ', 'Monumento al  municipio de Sultepec, las letras SULTEPEC ', 'https://maps.app.goo.gl/6xfXrgYa6Y1N2EAG7', '1700797011_LETRAS.jpg'),
+(8, 'Parques', 'PARQUE RECREATIVO EL MIRADOR', 'El parque perfecto para venir a disfrutar un día de campo junto a la familia, lugar con muchos arboles y juegos impresionantes. ', 'https://maps.app.goo.gl/7NchJaJ9GqTQn4XQ7', '1700797444_parque.jpeg'),
+(9, 'Parroquia', 'IGLESIA DE LA UNION', 'Una iglesia muy antigua con unas grandes decoraciones, una de las iglesias más bellas de Sultepec', 'https://maps.app.goo.gl/sxA9ngqvRfgi3u2t8', '1700798263_UNION.png'),
+(12, 'Parques', 'KIOSKO MUNICIPAL', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua”', 'https://maps.app.goo.gl/o9kJ7jEGgyqqmD6d6', '1701792237_KIOSK.jpg'),
+(13, 'Monumentos', 'MUSEO MUNICIPAL', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua”', '#', '1701792412_MUSEO.jpg'),
+(14, 'Parroquia', 'SANTUARIO SEÑOR DE LA SANTA VERACRUZ', 'Un santuario que data del siglo XIX y alberga una imagen del Cristo Negro a la que los devotos le atribuyen varios milagros.', 'https://maps.app.goo.gl/jDK1YovA2P7VLBFM9', '1701792577_igle.jpg'),
+(15, 'Mirador', ' EL MIRADOR', 'Un lugar de tranquilidad, con una excelente vista.', 'https://maps.app.goo.gl/xRKSsP7wLSSMSa3ZA', '1701792730_mira.jpg');
